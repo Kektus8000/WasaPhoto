f0940a719db594b6250fb20abeb6be6cd41b8cdf
Kektus8000 <149821405+Kektus8000@users.noreply.github.com>
2024-01-24 09:36:22 +0100

    Added Login View
    Added alpha version of the login view

---
+++ webui/src/views/LoginView.vue
@@ -0,0 +1,45 @@
+<template>
+    <body>
+      <h1> Effettua il Login</h1>
+
+      <input type = "text" placeholder="Inserisci il tuo username" v-model = "username">
+
+      <button :disabled="username.length < 6 || username.length > 20"
+      @click = "cercaUtente"> Login </button>
+
+      <h3 v-show= "username.length < 6 || username.length > 20"> L'username non è della lunghezza adatta </h3>
+    </body>
+  </template>
+  
+  <script>
+  export default{
+    data() {
+      return {
+        username : null,
+        errormsg : null,
+      }
+    },
+    methods: {
+      cercaUtente(){
+        try{
+            let response = this.$axios.get("/session");
+        }
+        catch(e)
+        {
+            this.errormsg = e.toString();
+        }
+        
+      }
+    }
+  }
+  </script>
+  
+  <style>
+    
+    body {
+      justify-content: center;
+      height: 200%;
+      display: grid;
+      place-items: center;
+    }
+  </style>
