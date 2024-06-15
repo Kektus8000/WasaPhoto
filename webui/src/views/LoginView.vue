bc224ad18fc2801caceb9eea4439eb0a9f6cf1d1
Kektus8000 <camagna.1997716@studenti.uniroma1.it>
2024-06-15 15:59:16 +0200

    Updated Login View

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -1,30 +1,3 @@
-catch(e)
-<template>
-  <title>Schermata Login</title>
-
-  <header>
-    <h2>Effettua il Login</h2>
-  </header>
-  
-  <hr>
-
-  <main>
-    <body class="principale">
-      <input type ="text" placeholder="Inserisci il tuo username" v-model="username">
-      <button class = "conferma" 
-      :disabled= "username.length > 16 || username.length < 3"
-      @click = "trovaUtente"> Effettua Login </button>
-    </body>
-    
-
-    <div class="fondo">
-      {{this.username}}
-    </div>
-  </main>
-
-
-</template>
-
 <script>
 export default{
   data(){
@@ -53,21 +26,85 @@ export default{
 }
 </script>
 
+<template>
+  <title>Schermata Login</title>
+
+  <body>
+    <img class = sfondo src= 'https://www.ifolor.ch/content/dam/ifolor/inspire-gallery/FFL/photoshop-ein-panorama-aus-einzelaufnahmen-erstellen/Panorama-580.jpg.transform/q60/image.jpg?Panorama-580.jpg'>
+    <h1 class = titolo >WasaPhoto</h1>
+    <input type ="text" placeholder="Inserisci il tuo username" v-model="username">
+    <button class= accesso :disabled= "username.length > 16 || username.length < 3"
+      @click = "trovaUtente">Accedi</button>
+  </body>
+  
+</template>
+
 <style>
-  header{
-    text-align: center;
-    top:0;
+  body{
+    width:100%;
+    height: 100%;
+    border: none;
+    position:absolute;
+    margin-left: auto;
+    margin-top: auto;
   }
 
-  .principale{
-    display: grid;
-    align-items:center;
-    justify-content: center;
-    width: 100%;
+  .sfondo{
+    position: absolute;
+    width:100%;
+    height:100%;
   }
-  .fondo{
-    bottom:0;
-    border-top: 1px solid black;
+
+  .titolo{
+    position: absolute;
+    transform: translate(-50%,-50%);
+
+    left: 50%;
+    top: 10%;
+    font-size: 800%;
+
+    -webkit-text-stroke-width: 0.5px;
+    -webkit-text-stroke-color: black;
+
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-style:italic;
+    color: white;
   }
+
+  .accesso{
+    position:absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+
+    width:200px;
+    height:30px;
+    
+    cursor: pointer;
+    text-align:center;
+    border-radius: 5px;
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    background-color: rgba(255, 255, 255, 0.9);
+  }
+
+  input{
+    position: absolute;
+    top: 43%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+
+    width:250px;
+    height:30px;
+    font-size: 25px;
+
+    cursor: pointer;
+    align-self:right;
+    border-radius: 5px;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: white;
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    text-align: center;
+  }
+
 
 </style>
