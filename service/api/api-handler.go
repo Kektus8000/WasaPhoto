6bf72eeca7c12b362aaef371f83a13f197601619
Kektus8000 <camagna.1997716@studenti.uniroma1.it>
2024-06-26 09:26:28 +0200

    Little api-handler update

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -7,54 +7,31 @@ import (
 // Handler returns an instance of httprouter.Router that handle APIs registered here
 func (rt *_router) Handler() http.Handler {
 
-	// Call the function that handle the User Login
-	rt.router.POST("/session", rt.wrap(rt.DoLogin))
-
-	// Call the function that handle the User Research
-	rt.router.PUT("/user/", rt.wrap(rt.SearchUsers))
-
-	//Call the function that handle the Set Username
-	rt.router.POST("/user/:userID/username", rt.wrap(rt.SetMyUsername))
-
-	// Call the function that handle the User Research
+	// Metodi GET
 	rt.router.GET("/userProfile/:userID", rt.wrap(rt.GetUserProfile))
-
-	//Call the function that handle the followUser
-	rt.router.POST("/userProfile/:userID/following/", rt.wrap(rt.FollowUser))
-
-	//Call the function that handle the unFollowUser
-	rt.router.DELETE("/userProfile/:userID/following/:followerID", rt.wrap(rt.UnfollowUser))
-
-	//Call the function that handle the getMyStream
 	rt.router.GET("/userProfile/:userID/stream/", rt.wrap(rt.GetMyStream))
 
-	//Call the function that handle the banUser
+	// Metodi POST
+	rt.router.POST("/session", rt.wrap(rt.DoLogin))
+	rt.router.POST("/user/:userID/username", rt.wrap(rt.SetMyUsername))
+	rt.router.POST("/userProfile/:userID/following/", rt.wrap(rt.FollowUser))
 	rt.router.POST("/userProfile/:userID/banList/", rt.wrap(rt.BanUser))
-
-	//Call the function that handle the unbanUser
-	rt.router.DELETE("/userProfile/:userID/banList/:bannedID", rt.wrap(rt.UnBanUser))
-
-	//Call the function that handle the uploadPhoto
-	rt.router.PUT("/userProfile/:userID/publishedPhotos/", rt.wrap(rt.UploadPhoto))
-
-	//Call the function that handle the deletePhoto
-	rt.router.DELETE("/userProfile/:userID/publishedPhotos/:photoID", rt.wrap(rt.DeletePhoto))
-
-	//Call the function that handle the likePhoto
 	rt.router.POST("/userProfile/:userID/stream/:photoID/likes/", rt.wrap(rt.LikePhoto))
 
-	//Call the function that handle the unlikePhoto
-	rt.router.DELETE("/userProfile/:userID/stream/:photoID/likes/", rt.wrap(rt.UnlikePhoto))
-
-	//Call the function that handle the commentPhoto
+	// Metodi PUT
+	rt.router.PUT("/user/", rt.wrap(rt.SearchUsers))
+	rt.router.PUT("/userProfile/:userID/publishedPhotos/", rt.wrap(rt.UploadPhoto))
 	rt.router.PUT("/userProfile/:userID/stream/:photoID/comments/", rt.wrap(rt.CommentPhoto))
 
-	//Call the function that handle the uncommentPhoto
+	// Metodi DELETE
+	rt.router.DELETE("/userProfile/:userID/following/:followerID", rt.wrap(rt.UnfollowUser))
+	rt.router.DELETE("/userProfile/:userID/banList/:bannedID", rt.wrap(rt.UnBanUser))
+	rt.router.DELETE("/userProfile/:userID/publishedPhotos/:photoID", rt.wrap(rt.DeletePhoto))
+	rt.router.DELETE("/userProfile/:userID/stream/:photoID/likes/", rt.wrap(rt.UnlikePhoto))
 	rt.router.DELETE("/userProfile/:userID/stream/:photoID/comments/:commentID", rt.wrap(rt.UncommentPhoto))
 
-	rt.router.GET("/context", rt.wrap(rt.getContextReply))
-
 	// Special routes
+	rt.router.GET("/context", rt.wrap(rt.getContextReply))
 	rt.router.GET("/liveness", rt.liveness)
 
 	return rt.router
