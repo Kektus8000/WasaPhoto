7f67d73ec75890b75176a855eed1fb5c1301ef27
Kektus8000 <camagna.1997716@studenti.uniroma1.it>
2024-07-04 09:44:19 +0200

    Removed fmt in LikePhoto.go

--- service/api/api-likePhoto.go
+++ service/api/api-likePhoto.go
@@ -3,7 +3,6 @@ package api
 import (
 	"net/http"
 	"strconv"
-	"fmt"
 
 	"github.com/Kektus8000/WasaPhoto/service/api/reqcontext"
 	"github.com/julienschmidt/httprouter"
@@ -15,7 +14,6 @@ func (rt *_router) LikePhoto(w http.ResponseWriter, r *http.Request, ps httprout
 	// Check dell'ID dell'Utente
 	userID := Authenticate(r.Header.Get("Authorization"))
 	if userID == -1 {
-		fmt.Println("Autorizzazione 1")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -23,21 +21,18 @@ func (rt *_router) LikePhoto(w http.ResponseWriter, r *http.Request, ps httprout
 	// Check dell'ID della Foto
 	photoID, errConv2 := strconv.Atoi(ps.ByName("photoID"))
 	if errConv2 != nil {
-		fmt.Println("Autorizzazione 2")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	publisherID, errQuery := rt.db.GetPhotoPublisher(photoID)
 	if errQuery != nil {
-		fmt.Println(errQuery)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	banned, errQuery2 := rt.db.CheckBanned(publisherID, userID)
 	if errQuery2 != nil {
-		fmt.Println(errQuery2)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	} else if banned == true {
@@ -47,7 +42,6 @@ func (rt *_router) LikePhoto(w http.ResponseWriter, r *http.Request, ps httprout
 
 	errUpdate := rt.db.LikePhoto(userID, photoID)
 	if errUpdate != nil {
-		fmt.Println(errUpdate)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
